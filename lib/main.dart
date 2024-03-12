79b93a1b4e1c3c175a8be0bd30b3505649186e13
Nicoline <nicolinemayanncortez@gmail.com>
2024-03-12 16:24:37 +0800

    nicoline added a code

--- lib/main.dart
+++ lib/main.dart
@@ -75,4 +75,46 @@ class _MyDictionaryPageState extends State<MyDictionaryPage> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blueAccent, // Set the color directly
                     ),
-
+                      ),
+                  const SizedBox(height: 16),
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Meaning:",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.pink,
+                              ),
+                            ),
+                            Text(
+                              definition.isNotEmpty ? definition : '',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            const SizedBox(height: 8),
+                            const Text(
+                              "Example:",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.pink,
+                              ),
+                            ),
+                            Text(
+                              example != null && example.isNotEmpty ? example : 'No Example Available',
+                              style: const TextStyle(fontSize: 16),
+                            ),
