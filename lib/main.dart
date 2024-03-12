8a27c4991c7ecb4f935e3676469166104df20730
noelGueco <gueconoel814@gmail.com>
2024-03-12 16:01:56 +0800

    noel added a code

--- lib/main.dart
+++ lib/main.dart
@@ -27,4 +27,52 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class MyDictionaryPage extends StatefulWidget {
+  const MyDictionaryPage({Key? key}) : super(key: key);
+
+  @override
+  _MyDictionaryPageState createState() => _MyDictionaryPageState();
+}
+
+class _MyDictionaryPageState extends State<MyDictionaryPage> {
+  TextEditingController wordController = TextEditingController();
+  String definition = "";
+  String example = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dictionary'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: wordController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter a word',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: onSearchPressed,
+                    child: Column(
+                      children: [
+                        const Text("Search"),
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent, // Set the color directly
+                    ),
 
